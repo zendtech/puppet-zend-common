21a8ef7c2bd5b676dd532911f9170498016a8084
Yeshua Hall <yeshua@sobo.red>
2022-10-27 14:16:21 -0500

    Use with_source instead

--- spec/classes/license_spec.rb
+++ spec/classes/license_spec.rb
@@ -10,7 +10,7 @@ describe 'zend_common::license' do
       let(:params) { { source: file_uri } }
 
       it { is_expected.to compile }
-      it { is_expected.to contain_class('zend_common::license').with(source: file_uri) }
+      it { is_expected.to contain_class('zend_common::license').with_source(file_uri) }
 
       describe 'creates license file' do
         it { is_expected.to contain_file('/opt/zend/zendphp/etc/license').with_source(file_uri) }
