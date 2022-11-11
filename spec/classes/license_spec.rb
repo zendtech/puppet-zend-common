277d78f3fc75de0378c14a94a2fbf7a57ae8a62a
Yeshua Hall <yeshua@sobo.red>
2022-11-11 11:28:15 -0600

    Add unit tests for license content

--- spec/classes/license_spec.rb
+++ spec/classes/license_spec.rb
@@ -6,14 +6,26 @@ describe 'zend_common::license' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       file_uri = 'puppet:///modules/test_module/licenses/test-license'
+      test_content = 'test-license-content'
       let(:facts) { os_facts }
-      let(:params) { { source: file_uri } }
 
-      it { is_expected.to compile }
-      it { is_expected.to contain_class('zend_common::license').with_source(file_uri) }
 
-      describe 'creates license file' do
+      describe 'without params' do
+        it { is_expected.to compile.and_raise_error(/.*/) }
+      end
+
+      describe 'with Puppet URL for source' do
+        let(:params) { { source: file_uri } }
+        it { is_expected.to compile }
+        it { is_expected.to contain_class('zend_common::license').with_source(file_uri) }
         it { is_expected.to contain_file('/opt/zend/zendphp/etc/license').with_source(file_uri) }
+      end
+      
+      describe 'with text for content' do
+        let(:params) { { content: test_content } }
+        it { is_expected.to compile }
+        it { is_expected.to contain_class('zend_common::license').with_content(test_content) }
+        it { is_expected.to contain_file('/opt/zend/zendphp/etc/license').with_content(test_content) }
       end
     end
   end
